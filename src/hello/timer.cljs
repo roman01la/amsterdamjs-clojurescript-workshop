7dfec85b784e0d09a52b5bd2837a3f31fb07ef64
Roman Liutikov <roman01la@romanliutikov.com>
2018-05-31 17:47:57 +0300

    add task description

--- src/hello/timer.cljs
+++ src/hello/timer.cljs
@@ -2,6 +2,13 @@
   (:require [reagent.core :as r]
             [goog.dom :as gdom]))
 
+;; Build Timer app that displays current time every second
+;; and allows changing the color of the text via input field
+
+;; Hints:
+;; - use `defonce` form to define a var that preserves it's value between hot-reloads
+;; - use `.toLocaleTimeString` method on `js/Date` object to format tim string
+
 (defn app []
   [:div
    "Render timer app here..."])
